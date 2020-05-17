4bb78fbc893cca2709458f95b7344794374d70dd
Claes Ö <>
2020-05-17 17:25:15 +0200

    1.1.0

--- PluginWfEmbed.php
+++ PluginWfEmbed.php
@@ -14,18 +14,33 @@ class PluginWfEmbed{
   public static function widget_embed($data){
     wfPlugin::includeonce('wf/array');
     $data = new PluginWfArray($data);
+    /**
+     * 
+     */
     if(!$data->get('data/type')){
       exit("PluginWfEmbed says: Param type is not set!");
     }
     if(!$data->get('data/file')){
       exit("PluginWfEmbed says: Param file is not set!");
     }
-    $filename = wfArray::get($GLOBALS, 'sys/app_dir').wfSettings::replaceTheme($data->get('data/file'));
-    if($data->get('data/file') && wfFilesystem::fileExist($filename)){
-      $element = wfDocument::createHtmlElement($data->get('data/type'), file_get_contents($filename));
+    /**
+     * 
+     */
+    $element = array();
+    $files = array();
+    if(!is_array($data->get('data/file'))){
+      $files[] = $data->get('data/file');
     }else{
-      exit("PluginWfEmbed says: File $filename does not exist!");
+      $files = $data->get('data/file');
     }
-    wfDocument::renderElement(array($element));
+    foreach ($files as $key => $value) {
+      $value = wfSettings::replaceTheme($value);
+      if($value && wfFilesystem::fileExist(wfGlobals::getAppDir().$value)){
+        $element[] = wfDocument::createHtmlElement($data->get('data/type'), file_get_contents(wfGlobals::getAppDir().$value));
+      }else{
+        exit("PluginWfEmbed says: File $value does not exist!");
+      }
+    }
+    wfDocument::renderElement($element);
   }
 }
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Buto-Plugin-WfEmbed
 Embed file content.
 
-Embed a file.
-
+## Embed
+Embed one file.
 ```
 type: widget
 data:
@@ -12,5 +12,16 @@ data:
     type: script
     file: /plugin/xx/yy/js/PluginXxYy.js
 ```
-
+Embed multiple files.
+```
+type: widget
+data:
+  plugin: 'wf/embed'
+  method: embed
+  data:
+    type: script
+    file: 
+      - /plugin/xx/yy/js/PluginXxYy1.js
+      - /plugin/xx/yy/js/PluginXxYy2.js
+```
 
